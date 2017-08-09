4a2053ec117072601db19bfdbc19e70350f2688a
Mario Esparza <marioesparza818@gmail.com>
2017-08-08 20:52:09 -0700

    docs for Github page

---
+++ docs/adventureConnie.js
@@ -0,0 +1,69 @@
+﻿
+/*Scroll Function*/
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+/*Nav Bar Resize Function*/
+$(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+$(document).ready(function () {
+    setMenuBindings();
+
+});
+function setMenuBindings() {
+    $("nav a").click(function (e) { /*e storage whereever "a" we click*/
+        e.preventDefault(); /*Prevent the ankor to work*/
+        var sectionID = e.currentTarget.id + "Section";
+
+        $("html body").animate({
+            scrollTop: $("#" + sectionID).offset().top
+
+        },1000) /*the animation it will happen in 1000 one second*/
+    })
+}
+
+
+jQuery(document).ready(function ($) {
+
+    $('#myCarousel').carousel({
+        interval: 5000
+    });
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+        var id = $('.item.active').data('slide-number');
+        $('#carousel-text').html($('#slide-content-' + id).html());
+    });
+});
+
+
+
